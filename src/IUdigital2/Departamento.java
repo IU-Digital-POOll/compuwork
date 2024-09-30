bfc5f67473ec56c65f265a8432129912db782a3f
SILVI COMPUWORK <silvia.posso@est.iudigital.edu.co>
2024-09-29 20:39:25 -0500

    Subiendo compuwork2

---
+++ src/IUdigital2/Departamento.java
@@ -0,0 +1,80 @@
+package IUdigital2;
+
+public class Departamento {
+    //crear atributos id,nombredepartamento,jefe,listaempleados,descripcion,gerente
+    private long  id;
+    private String nombreDepartamento;
+    private String jefe;
+    private String listaEmpleados;
+    private String descripcion;
+    private String gerente;
+
+    //crear constructor
+    public Departamento(long id, String nombreDepartamento, String jefe, String listaEmpleados, String descripcion, String gerente) {
+        this.id = id;
+        this.nombreDepartamento = nombreDepartamento;
+        this.jefe = jefe;
+        this.listaEmpleados = listaEmpleados;
+        this.descripcion = descripcion;
+        this.gerente = gerente;
+    }
+
+    //crear getters y setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreDepartamento() {
+        return nombreDepartamento;
+    }
+
+    public void setNombreDepartamento(String nombreDepartamento) {
+        this.nombreDepartamento = nombreDepartamento;
+    }
+
+    public String getJefe() {
+        return jefe;
+    }
+
+    public void setJefe(String jefe) {
+        this.jefe = jefe;
+    }
+
+    public String getListaEmpleados() {
+        return listaEmpleados;
+    }
+
+    public void setListaEmpleados(String listaEmpleados) {
+        this.listaEmpleados = listaEmpleados;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(String gerente) {
+        this.gerente = gerente;
+    }
+
+    //crear metodo para imprimir
+    public void imprimir() {
+        System.out.println("ID: " + id);
+        System.out.println("Nombre del Departamento: " + nombreDepartamento);
+        System.out.println("Jefe: " + jefe);
+        System.out.println("Lista de Empleados: " + listaEmpleados);
+        System.out.println("Descripcion: " + descripcion);
+        System.out.println("Gerente: " + gerente);
+    }
+}
---
+++ src/IUdigital2/ReporteDesempenio.java
@@ -0,0 +1,83 @@
+package IUdigital2;
+
+public class ReporteDesempenio {
+    //crear atributos id,empleados,fechareporte,puntajedesempenio,comentario,departamento
+    private long id;
+    private String empleados;
+    private String fechaReporte;
+    private int puntajeDesempenio;
+    private String comentario;
+    private String departamento;
+
+    //crear constructor
+    public ReporteDesempenio(long id, String empleados, String fechaReporte, int puntajeDesempenio, String comentario, String departamento) {
+        this.id = id;
+        this.empleados = empleados;
+        this.fechaReporte = fechaReporte;
+        this.puntajeDesempenio = puntajeDesempenio;
+        this.comentario = comentario;
+        this.departamento = departamento;
+    }
+
+    //crear getters y setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(String empleados) {
+        this.empleados = empleados;
+    }
+
+    public String getFechaReporte() {
+        return fechaReporte;
+    }
+
+    public void setFechaReporte(String fechaReporte) {
+        this.fechaReporte = fechaReporte;
+    }
+
+    public int getPuntajeDesempenio() {
+        return puntajeDesempenio;
+    }
+
+    public void setPuntajeDesempenio(int puntajeDesempenio) {
+        this.puntajeDesempenio = puntajeDesempenio;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    //crear metodo para imprimir
+    public void imprimir() {
+        System.out.println("ID: " + id);
+        System.out.println("Empleados: " + empleados);
+        System.out.println("Fecha de Reporte: " + fechaReporte);
+        System.out.println("Puntaje de Desempeño: " + puntajeDesempenio);
+        System.out.println("Comentario: " + comentario);
+        System.out.println("Departamento: " + departamento);
+    }
+
+
+
+}
